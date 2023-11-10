f6c2f013816eb247baeea141fcb5100ffde8678e
garret14deras <139816612+garret14deras@users.noreply.github.com>
2023-11-10 22:27:55 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,45 @@
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+
+}
+    data.forEach(item => {
+    });
+        console.log("Random Number: " + item);
+    }
+    const data = generateRandomData();
+}
+
+    return data;
+function main() {
+function generateRandomData() {
+main();
+
+    const data = [];        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+}
+    return data;
+        console.log("Random Number: " + item);
+
+main();
+
+function generateRandomData() {
+    data.forEach(item => {
+}
+    for (let i = 0; i < 10; i++) {
+function main() {
+    });
+    }
+
+    const data = generateRandomData();
+    return data
+
+if __name__ == "__main__":
+    main()
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    for item in data:
+import random
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
